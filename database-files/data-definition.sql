e60b2973828970019db78ceb5b8f36780761f453
gross-micah <grossmicah@Micahs-MacBook-Pro.local>
2017-05-24 15:19:46 -0700

    Changes to table creation w/ Heidi feedback

--- database-files/data-definition.sql
+++ database-files/data-definition.sql
@@ -1,14 +1,23 @@
-CREATE TABLE `user_search` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `image` blob,
-    `datetime` datetime,
-    PRIMARY KEY (`id`)
-)  ENGINE=INNODB;
+/*Ensure cleared state before creating*/
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `content_record`;
+DROP TABLE IF EXISTS `users`;
+SET FOREIGN_KEY_CHECKS=1;
+
+CREATE TABLE `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` varchar(255),
+  `email` varchar(320),
+)
 
 CREATE TABLE `content_record` (
     `id` INT NOT NULL AUTO_INCREMENT,
+    FOREIGN KEY (`user_id`) REFERENCES users(`id`)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
     `source` varchar(255),
-    `image` blob,
+    `type` varchar(255) NOT NULL CHECK (`type`='image' OR `type`='movie'),
+    `media` blob NOT NULL,
     `metadata` text,
     `location` varchar(255),
     `datetime_of_capture` datetime,
