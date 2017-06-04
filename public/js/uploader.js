c4edbddc11c5eb1cd0e1df2a1105dc1ab451fbea
Jason Hoffman <jasonandrewhoffman@gmail.com>
2017-06-04 17:00:37 +0000

    Images upload to /uploads correctly

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,13 +1,19 @@
-
-
 var multer = require('multer');
-var upload = multer({ dest: './uploads/' }).single('userimage');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+ 
+var upload = multer({ storage: storage }).single('userimage');
 
 module.exports.upload = function (req, res, next) {
   upload(req, res, function (err) {
-    if (err) return next(err)
-
-    console.log(req.file);
-  })
+    if (err) return next(err);
+    
+  });
 }
-
