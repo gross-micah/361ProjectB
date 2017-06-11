427dda7034f3ef3a239952eeba29f2eca61e1b62
gross-micah <gross.micah@gmail.com>
2017-06-11 10:54:26 -0700

    added blocking information

--- public/js/watsondetection.js
+++ public/js/watsondetection.js
@@ -18,3 +18,12 @@ visual_recognition.classify(params, function(err, res) {
   else
     console.log(JSON.stringify(res, null, 2));
 });
+
+//should return donalod trump with valid api key replacement
+// curl -XGET "https://gateway-a.watsonplatform.net/visual-recognition/api/v3/detect_faces?version=2016-05-20&api_key=$KEY&url=https://assets.donaldjtrump.com/2017/web/homepage/og_image.jpg"
+
+//currently troubleshooting invalid-api-key returns when everything is entered as expected.
+
+//emails to customer support still unanswered. Tracing problems in two URLs
+//https://stackoverflow.com/questions/38078487/ibm-watson-invalid-api-key
+//https://developer.ibm.com/answers/questions/282775/visual-recognition-no-longer-accepting-keys.html
